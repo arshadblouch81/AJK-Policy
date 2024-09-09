f0ea992c1da3dc050faeff0c333b06f4ee978063
arshadblouch81 <arshadblouch81@gmail.com>
2024-09-09 17:57:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,14 @@ from langchain_core.documents import Document
 from langchain_community.document_loaders import DirectoryLoader
 # Specify the path to your PDF file
 from langchain_community.document_loaders import PyPDFLoader
-#from langchain_chroma import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+
 
 from langchain_core.runnables import RunnableLambda
 from langchain_core.prompts import ChatPromptTemplate
@@ -17,6 +18,8 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.documents import Document
 import streamlit as st
 import os
+
+ 
 
 
 def get_file_text(pdf_path):
@@ -61,7 +64,8 @@ def get_file_text(pdf_path):
 def main():
     # Create a session state to keep track of whether the app is running
     
-     #model = ChatOpenAI(model="gpt-3.5-turbo")
+    api_key =  os.getenv("OPENAI_API_KEY")
+    #model = ChatOpenAI(model="gpt-3.5-turbo")
     MYPATH = "Kashmir Digital Policy 2024-2030 Sept 6.pdf"
     pdf_path = MYPATH
     documents = get_file_text(pdf_path)
@@ -70,18 +74,11 @@ def main():
         embedding=OpenAIEmbeddings(),
     )
 
-   
+
     retriever = RunnableLambda(vectorstore2.similarity_search).bind(k=1)  # select top result
 
     retriever.batch(["what is total population of AJK", "What is size of male and female population"])
-    #openai.api_key = st.secrets["openai"]["OPENAI_API_KEY"]
-    openai.api_key =  os.getenv("OPENAI_API_KEY")
-    #st.write(openai.api_key)
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.5,
-    max_tokens=1024,
-    timeout=None,
-    max_retries=2,
-    api_key=api_key)
+    llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
 
 
     message = """
@@ -96,8 +93,7 @@ def main():
     prompt = ChatPromptTemplate.from_messages([("human", message)])
 
     rag_chain = {"context": retriever, "question": RunnablePassthrough()} | prompt | llm
- 
-  
+
     
     # Create a session state to keep track of whether the app is running
     if 'running' not in st.session_state:
@@ -127,9 +123,8 @@ def main():
             st.session_state.running = False
         # Display the input
         if user_input and submit:          
-           #response = rag_chain.invoke(user_input)
-           #st.write(response.content)
-           st.write(use_input)
+            response = rag_chain.invoke(user_input)
+            st.write(response.content)
 
     # When the user clicks 'Exit', stop the loop
     else:
@@ -139,4 +134,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
