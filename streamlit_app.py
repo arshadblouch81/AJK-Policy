5b0205209b38cd21aec0355f7731497505c31c6a
arshadblouch81 <arshadblouch81@gmail.com>
2024-09-09 17:32:11 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 # Specify the path to your PDF file
 #from langchain_community.document_loaders import PyPDFLoader
 #from langchain_chroma import Chroma
-# from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
